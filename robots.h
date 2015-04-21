ddc8d746f2603faaa86d5c8b3edc5ab151e6aaf8
Sean M Southard <smsouth@hornet21.cs.clemson.edu>
2015-04-21 10:58:52 -0400

    updated prototypes 2.o

--- robots.h
+++ robots.h
@@ -15,9 +15,9 @@ enum commands {MOVE, TURN, STOP, IMAGE, GPS, DGPS, LASERS};
 
 
 
-void sendUDP(int sock, unsigned char *message, struct sockaddr_in serverAddr);
+void sendUDP(int sock, unsigned char *message, int size, struct sockaddr_in serverAddr);
 char *recvUDP(int sock, struct sockaddr_in allAddress);
 
-void sendTCP(int sock, char *message, struct sockaddr_in serverAddr);
+void sendTCP(int sock, char *message, int size, struct sockaddr_in serverAddr);
 char *recvTCP(int sock, struct sockaddr_in fromAddr);
 
